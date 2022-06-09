93243d8d73f64c3c6726e9a00e93c0998282c03a
Stuart Carruthers <stu@ironiclensflare.com>
2022-06-09 19:13:48 +0100

    Tweaked output

--- src/perfume-stats/Function.cs
+++ src/perfume-stats/Function.cs
@@ -21,9 +21,27 @@ public class Function
     {
         Console.WriteLine($"Found {items.Count} perfumes.");
         var perfumes = items.Select(p => new Perfume.Models.Perfume(p));
-        perfumes = perfumes.OrderBy(p => p.House).ThenBy(p => p.Name);
+        perfumes = perfumes.OrderByDescending(p => p.Wearings?.Count ?? 0).ThenBy(p => p.House).ThenBy(p => p.Name);
         Console.WriteLine($"Mapped {perfumes.Count()} perfumes.");
-        var stats = perfumes.Select(p => $"House: {p.House}\nName: {p.Name}\nWearings: {p.Wearings?.Count ?? 0}\nLast worn: {p.Wearings?.Last().ToString("dd MMMM yyyy")}").ToArray();
-        return string.Join("\n\n", stats);
+        var stats = perfumes.Select(p => BuildPerfumeEntry(p));
+        return string.Join('\n', stats);
+    }
+
+    private string BuildPerfumeEntry(Perfume.Models.Perfume perfume)
+    {
+        var writer = new StringWriter();
+        var wearings = perfume.Wearings != null ? perfume.Wearings.Count.ToString() : "Not worn";
+        var lastWorn = perfume.Wearings?.Last().ToString("dd/MM/yyyy") ?? "Never";
+        writer.WriteLine(WriteEntry("House", perfume.House));
+        writer.WriteLine(WriteEntry("Name:", perfume.Name));
+        writer.WriteLine(WriteEntry("Wearings:", wearings));
+        writer.WriteLine(WriteEntry("Last worn:", lastWorn));
+        writer.Write(new string('-', 32));
+        return writer.ToString();
+    }
+
+    private string WriteEntry(string key, string value)
+    {
+        return $"{key}{value.PadLeft(32-key.Length)}";
     }
 }
